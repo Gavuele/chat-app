d2ac80cd0fe83a1807279dd9a09c26ae86b7784a
Your Name <you@example.com>
2024-03-22 16:49:34 +0300

    global fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 const __dirname = path.resolve();
 
