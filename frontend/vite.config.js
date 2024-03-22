72a9356a1ed2df79c73428c2929b61aa4b6c3dcd
Your Name <you@example.com>
2024-03-22 16:33:39 +0300

    Fix deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  build: {
+    outDir: 'dist'
+  }, // Добавлена запятая здесь
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
 });
